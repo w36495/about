27584eef2eb41cd7810357099858ab838a0c83b6
w36495 <w36495@naver.com>
2022-08-09 23:46:08 +0900

    [feat/ABOUT-9] Room Database Entity Setting

--- app/src/main/java/com/w36495/about/data/Think.kt
+++ app/src/main/java/com/w36495/about/data/Think.kt
@@ -1,10 +1,14 @@
 package com.w36495.about.data
 
-import java.util.*
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "thinks")
 data class Think(
-    val id: Long,
+    val topicId: Long,
     val text: String,
     val registDate: String,
     val updateDate: String
-)
+) {
+    @PrimaryKey(autoGenerate = true) var id: Long = 0
+}
