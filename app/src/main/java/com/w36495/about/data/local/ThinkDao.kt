56893b4625272db18a026ebdbc3bd26e73988007
w36495 <w36495@naver.com>
2022-08-21 01:04:34 +0900

    [feat/ABOUT-15] ThinkDao 메소드 suspend 로 변경

--- app/src/main/java/com/w36495/about/data/local/ThinkDao.kt
+++ app/src/main/java/com/w36495/about/data/local/ThinkDao.kt
@@ -6,23 +6,23 @@ import com.w36495.about.data.Think
 @Dao
 interface ThinkDao {
     @Query("SELECT * FROM thinks WHERE topicId = :topicId")
-    fun getThinkListByTopicId(topicId: Long): List<Think>
+    suspend fun getThinkListByTopicId(topicId: Long): List<Think>
 
     @Query("SELECT * FROM thinks WHERE id = :thinkId")
-    fun getThinkById(thinkId: Long): Think
+    suspend fun getThinkById(thinkId: Long): Think
 
     @Query("SELECT count(*) FROM thinks WHERE topicId = :topicId")
     fun getThinkListCountByTopicId(topicId: Long): Int
 
     @Update
-    fun updateThink(think: Think)
+    suspend fun updateThink(think: Think)
 
     @Insert
-    fun insertThink(think: Think)
+    suspend fun insertThink(think: Think)
 
     @Query("DELETE FROM thinks WHERE topicId = :topicId")
-    fun deleteThinkByTopicId(topicId: Long)
+    suspend fun deleteThinkByTopicId(topicId: Long)
 
     @Query("DELETE FROM thinks WHERE id = :thinkId")
-    fun deleteThinkById(thinkId: Long)
+    suspend fun deleteThinkById(thinkId: Long)
 }
