667163427b587e7bb5233258e52db77450e3d28d
w36495 <w36495@naver.com>
2022-10-10 20:44:00 +0900

    [feat/ABOUT-31] DataFormat 코드 수정

--- app/src/main/java/com/w36495/about/util/DateFormat.kt
+++ app/src/main/java/com/w36495/about/util/DateFormat.kt
@@ -3,20 +3,23 @@ package com.w36495.about.util
 import java.text.SimpleDateFormat
 import java.util.*
 
-private val current = System.currentTimeMillis()
-private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+class DateFormat {
+    companion object {
+        private val current = System.currentTimeMillis()
+        private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
-fun currentDateFormat(): String {
-    val date = Date(current)
-    return dateFormat.format(date)
-}
+        fun currentDateFormat(): String {
+            val date = Date(current)
+            return dateFormat.format(date)
+        }
 
-fun calDate(topicDate: String): Long {
+        fun calDate(topicDate: String): Long {
+            val current = System.currentTimeMillis()
+            val currentDate = Date(current)
 
-    val current = System.currentTimeMillis()
-    val currentDate = Date(current)
+            val calculatorMillis = currentDate.time - dateFormat.parse(topicDate).time
 
-    val calculatorMillis = currentDate.time - dateFormat.parse(topicDate).time
-
-    return calculatorMillis / (60 * 60 * 24 * 1000)
+            return calculatorMillis / (60 * 60 * 24 * 1000)
+        }
+    }
 }
