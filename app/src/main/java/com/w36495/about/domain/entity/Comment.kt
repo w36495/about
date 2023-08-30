9e181fb8f9dcaa9361b4591c3b7ea35cc5b8e810
w36495 <w36495@naver.com>
2023-08-30 17:32:55 +0900

    [feat/ABOUT-49] Comment Entity 생성

---
+++ app/src/main/java/com/w36495/about/domain/entity/Comment.kt
@@ -0,0 +1,13 @@
+package com.w36495.about.domain.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "comments")
+data class Comment(
+    @PrimaryKey
+    val id: Long,
+    val thinkId: Long,
+    val comment: String,
+    val registDate: String,
+)
