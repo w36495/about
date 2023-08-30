4b7bc03f9d2a285927892bf1c9ae20988380fd6f
w36495 <w36495@naver.com>
2023-08-30 17:34:27 +0900

    [feat/ABOUT-49] Room Database Version Increase & Database Migration (from 1 to 2)

--- app/src/main/java/com/w36495/about/data/local/AppDatabase.kt
+++ app/src/main/java/com/w36495/about/data/local/AppDatabase.kt
@@ -2,20 +2,39 @@ package com.w36495.about.data.local
 
 import android.content.Context
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.w36495.about.domain.entity.Comment
 import com.w36495.about.domain.entity.Think
 import com.w36495.about.domain.entity.Topic
 
 @Database(
-    version = 1,
-    entities = [Topic::class, Think::class]
+    version = 2,
+    entities = [Topic::class, Think::class, Comment::class]
 )
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun topicDao(): TopicDao
     abstract fun thinkDao(): ThinkDao
+    abstract fun commentDao(): CommentDao
 
     companion object {
         private var instance: AppDatabase? = null
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    """
+                        CREATE TABLE comments (
+                        id INTEGER PRIMARY KEY NOT NULL,
+                        thinkId INTEGER NOT NULL,
+                        comment TEXT NOT NULL DEFAULT '',
+                        registDate TEXT NOT NULL DEFAULT ''
+                        )
+                    """.trimMargin()
+                )
+            }
+        }
 
         fun getInstance(context: Context): AppDatabase? {
             if (instance == null) {
@@ -25,6 +44,8 @@ abstract class AppDatabase : RoomDatabase() {
                         AppDatabase::class.java,
                         "About"
                     )
+                        .setJournalMode(JournalMode.TRUNCATE)
+                        .addMigrations(MIGRATION_1_2)
                         .build()
                 }
             }
