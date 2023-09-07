09f20a9cff772e4f617051211798c23d8180f57f
w36495 <w36495@naver.com>
2023-09-07 21:36:38 +0900

    [feat/ABOUT-49] 파일명 수정 (1) ThinkContract -> ThinkListContract (2) ThinkPresenter -> ThinkListPresenter (3) ThinkFragment -> ThinkListFragment

---
+++ app/src/main/java/com/w36495/about/contract/ThinkListContract.kt
@@ -0,0 +1,22 @@
+package com.w36495.about.contract
+
+import com.w36495.about.domain.entity.Think
+
+interface ThinkListContract {
+
+    interface View {
+        fun showThinkList(thinkList: List<Think>)
+        fun showThink(think: Think)
+        fun showToast(message: String)
+        fun showError(message: String)
+    }
+
+    interface Presenter {
+        fun getThink(id: Long)
+        fun getThinkList(topicId: Long)
+        fun saveThink(think: Think)
+        fun updateThink(think: Think)
+        fun deleteThinkById(id: Long)
+    }
+
+}
