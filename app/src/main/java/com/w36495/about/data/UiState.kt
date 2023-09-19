5030a6150d733dc24db064455e1a3bcacfaae3a0
w36495 <w36495@naver.com>
2023-09-19 21:59:19 +0900

    [refactor/ABOUT-61] 각 화면에 필요한 UiState 구현

--- app/src/main/java/com/w36495/about/data/UiState.kt
+++ app/src/main/java/com/w36495/about/data/UiState.kt
@@ -1,13 +1,21 @@
 package com.w36495.about.data
 
+import com.w36495.about.domain.dto.TopicListDTO
+import com.w36495.about.domain.entity.Comment
 import com.w36495.about.domain.entity.Think
 import com.w36495.about.domain.entity.Topic
 
 sealed class TopicUiState {
     object Loading : TopicUiState()
-    object Empty : TopicUiState()
     data class Success(val list: List<Topic>) : TopicUiState()
     data class Failed(val message: String?) : TopicUiState()
+}
+
+sealed class TopicListUiState {
+    object Loading : TopicListUiState()
+    object Empty : TopicListUiState()
+    data class Success(val topicList: List<TopicListDTO>) : TopicListUiState()
+    data class Failed(val tag: String, val message: String) : TopicListUiState()
 }
 
 sealed class ThinkUiState {
@@ -15,4 +23,11 @@ sealed class ThinkUiState {
     object Empty : ThinkUiState()
     data class Success(val thinkList: List<Think>) : ThinkUiState()
     data class Failed(val message: String?) : ThinkUiState()
+}
+
+sealed class CommentUiState {
+    object Loading : CommentUiState()
+    object Empty : CommentUiState()
+    data class Success(val thinkList: List<Comment>) : CommentUiState()
+    data class Failed(val message: String?) : CommentUiState()
 }
