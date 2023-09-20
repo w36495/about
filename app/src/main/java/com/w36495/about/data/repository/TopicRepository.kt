1810db3fdf7856239067a0cf86f278da544f3d72
w36495 <w36495@naver.com>
2023-09-20 14:40:59 +0900

    [bug/ABOUT-56] Topic Repository 수정

--- app/src/main/java/com/w36495/about/data/repository/TopicRepository.kt
+++ app/src/main/java/com/w36495/about/data/repository/TopicRepository.kt
@@ -1,13 +1,14 @@
 package com.w36495.about.data.repository
 
+import com.w36495.about.domain.dto.TopicListDTO
 import com.w36495.about.domain.entity.Topic
 import kotlinx.coroutines.flow.Flow
 
 interface TopicRepository {
 
-    suspend fun getTopicById(topicId: Long): Topic
+    suspend fun getTopicById(topicId: Long): Flow<Topic>
 
-    suspend fun getTopicList(): Flow<List<Topic>>
+    suspend fun getTopicList(): Flow<List<TopicListDTO>>
 
     suspend fun insertTopic(topic: Topic): String
 
--- app/src/main/java/com/w36495/about/data/repository/TopicRepositoryImpl.kt
+++ app/src/main/java/com/w36495/about/data/repository/TopicRepositoryImpl.kt
@@ -1,15 +1,16 @@
 package com.w36495.about.data.repository
 
 import com.w36495.about.data.local.TopicDao
+import com.w36495.about.domain.dto.TopicListDTO
 import com.w36495.about.domain.entity.Topic
 import kotlinx.coroutines.flow.Flow
 
 class TopicRepositoryImpl(private val topicDao: TopicDao) : TopicRepository {
-    override suspend fun getTopicById(topicId: Long): Topic {
+    override suspend fun getTopicById(topicId: Long): Flow<Topic> {
         return topicDao.getTopicById(topicId)
     }
 
-    override suspend fun getTopicList(): Flow<List<Topic>> {
+    override suspend fun getTopicList(): Flow<List<TopicListDTO>> {
         return topicDao.getTopicList()
     }
 
